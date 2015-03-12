123c7c4ae43400e4751a36f9183f17425b6ec654
hm <hmkuak@naver.com>
2015-03-12 20:20:13 +0900

    CreativeEconomy 중간코딩

--- CreativeEconomy/src/CreativeEconomy/CreativeEconomy.php
+++ CreativeEconomy/src/CreativeEconomy/CreativeEconomy.php
@@ -14,12 +14,19 @@ use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat;
 use pocketmine\block\Block;
+use pocketmine\network\protocol\AddItemEntityPacket;
+use pocketmine\network\protocol\MoveEntityPacket;
+use pocketmine\entity\Entity;
+use pocketmine\level\Position;
+use pocketmine\item\Item;
 
 class CreativeEconomy extends PluginBase implements Listener {
 	private static $instance = null;
-	public $messages, $db, $marketCount;
+	public $messages, $db;
+	public $marketCount, $marketPrice, $itemName;
 	public $economyAPI = null;
 	public $purchaseQueue = [ ];
+	public $createQueue = [ ];
 	public function onEnable() {
 		@mkdir ( $this->getDataFolder () );
 		
@@ -27,16 +34,15 @@ class CreativeEconomy extends PluginBase implements Listener {
 		
 		$this->saveResource ( "marketPrice.yml", false );
 		$this->saveResource ( "marketCount.yml", false );
-		$this->saveResource ( "en_item_data.yml", false );
-		if ($this->messages ["default-language"] != "en") $this->saveResource ( $this->messages ["default-language"] . "_item_data.yml", false );
+		$this->saveResource ( $this->messages ["default-language"] . "_item_data.yml", false );
 		
-		$this->saveDefaultConfig ();
-		$this->reloadConfig ();
-		
-		$this->db = (new Config ( $this->getDataFolder () . "marketDB.yml", Config::YAML ))->getAll ();
+		$this->db = (new Config ( $this->getDataFolder () . "marketDB.yml", Config::YAML, [ 
+				"allow-purchase" => true ] ))->getAll ();
 		$this->marketCount = (new Config ( $this->getDataFolder () . "marketCount.yml", Config::YAML ))->getAll ();
+		$this->marketPrice = (new Config ( $this->getDataFolder () . "marketPrice.yml", Config::YAML ))->getAll ();
+		$this->itemName = (new Config ( $this->getDataFolder () . $this->messages ["default-language"] . "_item_data.yml", Config::YAML ))->getAll ();
 		
-		if ($this->checkEconomyAPI ()) {
+		if ($this->getServer ()->getPluginManager ()->getPlugin ( "EconomyAPI" ) === null) {
 			$this->economyAPI = \onebone\economyapi\EconomyAPI::getInstance ();
 		} else {
 			$this->getLogger ()->error ( $this->get ( "there-are-no-economyapi" ) );
@@ -69,10 +75,23 @@ class CreativeEconomy extends PluginBase implements Listener {
 				$this->alert ( $player, $this->get ( "ur-not-use-market" ) );
 				return;
 			}
+			if ($this->db ["allow-purchase"] == false) {
+				$this->alert ( $player, $this->get ( "ur-not-use-market" ) );
+				return;
+			}
 			$this->purchaseQueue [$event->getPlayer ()->getName ()] ["id"] = $this->db ["signMarket"] ["{$block->x}:{$block->y}:{$block->z}"];
 			$this->message ( $event->getPlayer (), $this->get ( "you-can-buy-or-sell" ) );
 			$this->message ( $event->getPlayer (), $this->get ( "buy-or-sell-help-command" ) );
 			$event->setCancelled ();
+		}
+		if (isset ( $this->createQueue [$event->getPlayer ()->getName ()] )) {
+			$event->setCancelled ();
+			$pos = $block->getSide ( 0 );
+			$item = $this->createQueue [$event->getPlayer ()->getName ()];
+			$this->db ["signMarket"] ["{$pos->x}:{$pos->y}:{$pos->z}"] = $item;
+			$block->getLevel ()->setBlock ( $pos, Block::get ( Item::GLASS ), true );
+			unset ( $this->createQueue [$event->getPlayer ()->getName ()] );
+			$this->message ( $player, $this->get ( "market-completely-created" ) );
 		}
 	}
 	public function onBreak(BlockBreakEvent $event) {
@@ -109,7 +128,7 @@ class CreativeEconomy extends PluginBase implements Listener {
 				}
 				switch ($args [0]) {
 					case $this->get ( "sub-commands-create" ) :
-						(isset ( $args [1] )) ? $this->CreativeEconomy ( $player, $args [1] ) : $this->CreativeEconomy ( $player );
+						(isset ( $args [1] )) ? $this->CECreateQueue ( $player, $args [1] ) : $this->CECreateQueue ( $player );
 						break;
 					case $this->get ( "sub-commands-autocreate" ) :
 						$this->CEAutoSet ( $player );
@@ -131,26 +150,95 @@ class CreativeEconomy extends PluginBase implements Listener {
 		}
 		return true;
 	}
-	public function CEBuyCommand(Player $player, $count = 1) {
-		if (! isset ( $this->purchaseQueue [$player->getName ()] )) {
+	public function CEBuyCommand(Player $player, $count = 1, $item = null) {
+		if ($this->db ["allow-purchase"] == false) {
+			$this->alert ( $player, $this->get ( "ur-not-use-market" ) );
+			return;
+		}
+		if (! isset ( $this->purchaseQueue [$player->getName ()] ) and $item == null) {
 			$this->message ( $player, $this->get ( "please-choose-item" ) );
 			return;
 		} else {
-			// TODO 아이템에 따라 구매처리
+			if ($item == null) $item = $this->purchaseQueue [$player->getName ()];
+			if (! isset ( $this->marketPrice [$item] )) {
+				$this->alert ( $player, $this->get ( "not-found-item-data" ) );
+				return;
+			}
+			$price = $this->marketPrice [$item] * $count;
+			$check = explode ( ".", $item );
+			if (! is_numeric ( $count ) and ! isset ( $check [1] )) {
+				$this->alert ( $player, $this->get ( "buy-or-sell-help-command" ) );
+				return;
+			}
+			if (! isset ( $check [1] )) $check [1] = 0;
+			$money = $this->economyAPI->myMoney ( $player );
+			if ($money < $price) {
+				$this->alert ( $player, $this->get ( "not-enough-money-to-purchase" ) );
+				return;
+			}
+			$this->economyAPI->reduceMoney ( $player, $price );
+			$player->getInventory ()->addItem ( Item::get ( $check [0], $check [1], $count ) );
+			(! isset ( $this->itemName [$item] )) ? $itemName = "undefied" : $itemName = $this->itemName [$item];
+			$this->message ( $player, $itemName . "({$item})({$count}) " . $this->get ( "is-successfully-buyed" ) );
+			if (isset ( $this->purchaseQueue [$player->getName ()] )) unset ( $this->purchaseQueue [$player->getName ()] );
 			return;
 		}
 	}
 	public function CESellCommand(Player $player, $count = 1) {
-		if (! isset ( $this->purchaseQueue [$player->getName ()] )) {
+		if ($this->db ["allow-purchase"] == false) {
+			$this->alert ( $player, $this->get ( "ur-not-use-market" ) );
+			return;
+		}
+		if (! isset ( $this->purchaseQueue [$player->getName ()] ) and $item == null) {
 			$this->message ( $player, $this->get ( "please-choose-item" ) );
 			return;
 		} else {
-			// TODO 아이템에 따라 판매처리
+			if ($item == null) $item = $this->purchaseQueue [$player->getName ()];
+			if (! isset ( $this->marketPrice [$item] )) {
+				$this->alert ( $player, $this->get ( "not-found-item-data" ) );
+				return;
+			}
+			$price = $this->marketPrice [$item] * $count;
+			$check = explode ( ".", $item );
+			if (! is_numeric ( $count ) and ! isset ( $check [1] )) {
+				$this->alert ( $player, $this->get ( "buy-or-sell-help-command" ) );
+				return;
+			}
+			if (! isset ( $check [1] )) $check [1] = 0;
+			$haveItem = 0;
+			foreach ( $player->getInventory ()->getContents () as $inven ) {
+				if (! $inven instanceof Item) return;
+				if ($inven->getID () == $check [0] and $inven->getDamage () == $check [1]) {
+					$haveItem = $inven->getCount ();
+				}
+			}
+			if ($haveItem < $count) {
+				$this->alert ( $player, $this->get ( "not-enough-item" ) );
+				return;
+			}
+			$this->economyAPI->addMoney ( $player, $price );
+			$player->getInventory ()->addItem ( Item::get ( $check [0], $check [1], $count ) );
+			(! isset ( $this->itemName [$item] )) ? $itemName = "undefied" : $itemName = $this->itemName [$item];
+			$this->message ( $player, $itemName . "({$item})({$count}) " . $this->get ( "is-successfully-selled" ) );
+			if (isset ( $this->purchaseQueue [$player->getName ()] )) unset ( $this->purchaseQueue [$player->getName ()] );
 			return;
 		}
 	}
-	public function CreativeEconomy(Player $player, $item = null) {
-		// TODO 터치한 위치에 자동 쇼케이스 작업기능
+	public function CECreateQueue(Player $player, $item = null) {
+		if ($item == null or ! is_numeric ( $item )) {
+			$this->alert ( $player, $this->get ( "commands-ce-help1" ) );
+			return;
+		}
+		$this->message ( $player, $this->get ( "which-you-want-place-choose-pos" ) );
+		$this->createQueue [$player->getName ()] = $item;
+	}
+	public function CreativeEconomy() {
+		// 스케쥴로 매번 위치확인하면서 생성작업시작
+		// 아예 별도로 생성확인변수도 만들어서
+		// 위치에 맞게 생성패킷을 보냈는지 체크 후
+		// 생성되지않았을경우(새로생기거나 유저가 이동했거나)
+		// 해당 생성패킷을 보내고 =1 처리하도록 처리
+		// 만약에 멀어진 경우 생성해제 패킷을 보내고 =0처리
 	}
 	public function CEAutoSet() {
 		// TODO 1줄 전자동 상점설치
@@ -159,13 +247,16 @@ class CreativeEconomy extends PluginBase implements Listener {
 		// TODO 기본가격시세를 입력된 값으로 설정
 	}
 	public function AllFreezeMarket() {
-		// TODO 상점매매를 모두 허용/차단
+		if ($this->db ["allow-purchase"] == true) {
+			$this->db ["allow-purchase"] = false;
+			$this->message ( $player, $this->get ( "market-enabled" ) );
+		} else {
+			$this->db ["allow-purchase"] = true;
+			$this->message ( $player, $this->get ( "market-disabled" ) );
+		}
 	}
 	public function get($var) {
 		return $this->messages [$this->messages ["default-language"] . "-" . $var];
-	}
-	public function checkEconomyAPI() {
-		return (($this->getServer ()->getLoader ()->findClass ( 'onebone\\economyapi\\EconomyAPI' )) == null) ? false : true;
 	}
 	public function initMessage() {
 		$this->saveResource ( "messages.yml", false );
