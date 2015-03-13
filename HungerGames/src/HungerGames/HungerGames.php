47e99ccb5fc0cdc3ecb0c42db34d6be02a80b39c
Administrator <Administrator@HERA144>
2015-03-14 05:19:33 +0900

    HungerGames 80% 작업진행

--- HungerGames/src/HungerGames/HungerGames.php
+++ HungerGames/src/HungerGames/HungerGames.php
@@ -16,13 +16,23 @@ use pocketmine\network\protocol\UpdateBlockPacket;
 use pocketmine\block\Block;
 use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\scheduler\CallbackTask;
+use pocketmine\event\entity\ProjectileLaunchEvent;
+use pocketmine\entity\Arrow;
+use pocketmine\event\Event;
+use pocketmine\event\entity\ExplosionPrimeEvent;
+use pocketmine\level\Explosion;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageByChildEntityEvent;
 
 class HungerGames extends PluginBase implements Listener {
-	public $config, $configData;
+	public $settings, $score;
 	public $m_version = 1;
 	public $hungerItem = [ ], $hungerItemName = [ ];
 	public $armorItem = [ ], $armorItemName = [ ];
-	public $touchedQueue = [ ];
+	public $touchedQueue = [ ], $fireQueue = [ ];
+	public $attackQueue = [ ];
 	public $updatePk;
 	public function onEnable() {
 		@mkdir ( $this->getDataFolder () );
@@ -30,16 +40,21 @@ class HungerGames extends PluginBase implements Listener {
 		$this->initMessage ();
 		$this->messagesUpdate ();
 		
-		$this->config = new Config ( $this->getDataFolder () . "settings.yml", Config::YAML, [ ] );
-		$this->configData = $this->config->getAll ();
+		$this->settings = (new Config ( $this->getDataFolder () . "settings.yml", Config::YAML, [ ] ))->getAll ();
+		$this->score = (new Config ( $this->getDataFolder () . "hunger_data.yml", Config::YAML ))->getAll ();
 		
 		$this->updatePk = new UpdateBlockPacket ();
 		$this->updatePk->meta = 0;
 		$this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
 	}
 	public function onDisable() {
-		$this->config->setAll ( $this->configData );
-		$this->config->save ();
+		$this->save = new Config ( $this->getDataFolder () . "settings.yml", Config::YAML );
+		$this->save->setAll ( $this->settings );
+		$this->save->save ();
+		
+		$this->save = new Config ( $this->getDataFolder () . "hunger_data.yml", Config::YAML );
+		$this->save->setAll ( $this->score );
+		$this->save->save ();
 	}
 	public function onTouch(PlayerInteractEvent $event) {
 		$block = $event->getBlock ();
@@ -54,7 +69,7 @@ class HungerGames extends PluginBase implements Listener {
 			}
 			$index = rand ( 0, count ( $this->hungerItem ) - 1 );
 			$this->touchedQueue [$player->getName ()] [$blockPos] = 0;
-			$this->setGlowObsidian ( $player, $block->x, $block->y, $block->z );
+			$this->setBlockPacket ( $player, $block->x, $block->y, $block->z, Block::GLOWING_OBSIDIAN );
 			
 			$armorRand = rand ( 1, 5 );
 			if ($armorRand == 1) {
@@ -81,31 +96,61 @@ class HungerGames extends PluginBase implements Listener {
 			$player->setHealth ( $player->getHealth () + 2 );
 			$this->message ( $player, $this->get ( "successfully-cared" ) );
 		}
+		foreach ( $this->hungerItem as $index => $hungerItem ) {
+			if ($index > 8) break;
+			if ($event->getItem ()->getId () == $hungerItem->getId ()) {
+				$block = $block->getSide ( 0 );
+				$this->fireblock [$blockPos] ["id"] = $block->getId ();
+				$this->fireblock [$blockPos] ["damage"] = $block->getDamage ();
+				
+				if (! isset ( $this->fireblock [$blockPos] )) {
+					foreach ( $this->getServer ()->getOnlinePlayers () as $player )
+						$this->setBlockPacket ( $player, $block->x, $block->y, $block->z, Block::FIRE );
+					$this->getServer ()->getScheduler ()->scheduleDelayedTask ( new CallbackTask ( [ 
+							$this,
+							"restoreBlock" ], [ 
+							$block->x,
+							$block->y,
+							$block->z ] ), 80 );
+				} else {
+					$this->restoreBlock ( $block->x, $block->y, $block->z );
+				}
+				break;
+			}
+		}
+	}
+	public function checkArrow(ProjectileLaunchEvent $event) {
+		if ($event->getEntity () instanceof Arrow) {
+			$this->getServer ()->getScheduler ()->scheduleDelayedTask ( new CallbackTask ( [ 
+					$this,
+					"removeArrow" ], [ 
+					$event ] ), 20 );
+		}
+	}
+	public function removeArrow(Event $event) {
+		if ($event->isCancelled ()) return;
 		
-		// TODO FIRE
+		$arrow = $event->getEntity ();
+		$murder = $event->getEntity ()->shootingEntity;
+		
+		$this->getServer ()->getPluginManager ()->callEvent ( $ev = new ExplosionPrimeEvent ( $arrow, 3.2 ) );
+		if (! $ev->isCancelled ()) {
+			$explosion = new Explosion ( $arrow, $ev->getForce (), $murder );
+			$explosion->explodeB ();
+		}
+		
+		$reflection_class = new \ReflectionClass ( $arrow );
+		$property = $reflection_class->getProperty ( 'age' );
+		$property->setAccessible ( true );
+		$property->setValue ( $arrow, 7000 );
 	}
-	public function setGlowObsidian(Player $player, $x, $y, $z) {
-		$this->updatePk->x = $x;
-		$this->updatePk->y = $y;
-		$this->updatePk->z = $z;
-		$this->updatePk->block = Block::GLOWING_OBSIDIAN;
-		$player->dataPacket ( $this->updatePk );
+	public function restoreBlock($x, $y, $z) {
+		if (! isset ( $this->fireblock ["{$x}:{$y}:{z}"] )) return;
+		foreach ( $this->getServer ()->getOnlinePlayers () as $player )
+			$this->setBlockPacket ( $player, $x, $y, $z, $this->fireblock ["{$x}.{$y}.{z}"] ["id"], $this->fireblock ["{$x}.{$y}.{z}"] ["damage"] );
+		unset ( $this->fireblock ["{$x}.{$y}.{z}"] );
 	}
-	public function setDiamondBlock(Player $player, $x, $y, $z) {
-		$this->updatePk->x = $x;
-		$this->updatePk->y = $y;
-		$this->updatePk->z = $z;
-		$this->updatePk->block = Block::DIAMOND_BLOCK;
-		$player->dataPacket ( $this->updatePk );
-	}
-	public function setFireBlock(Player $player, $x, $y, $z) {
-		$this->updatePk->x = $x;
-		$this->updatePk->y = $y;
-		$this->updatePk->z = $z;
-		$this->updatePk->block = Block::FIRE;
-		$player->dataPacket ( $this->updatePk );
-	}
-	public function restoreBlock(Player $player, $x, $y, $z, $block, $meta) {
+	public function setBlockPacket(Player $player, $x, $y, $z, $block, $meta = 0) {
 		$this->updatePk->x = $x;
 		$this->updatePk->y = $y;
 		$this->updatePk->z = $z;
@@ -119,7 +164,7 @@ class HungerGames extends PluginBase implements Listener {
 		foreach ( $this->hungerItem as $hungerItem )
 			$player->getInventory ()->remove ( $hungerItem );
 		
-		$air = new Item ( 0 );
+		$air = Item::get ( Item::AIR );
 		$player->getInventory ()->setArmorContents ( [ 
 				$air,
 				$air,
@@ -127,13 +172,42 @@ class HungerGames extends PluginBase implements Listener {
 				$air ] );
 		$player->getInventory ()->sendArmorContents ( $player );
 	}
+	public function onAttack(EntityDamageEvent $event) {
+		if ($event instanceof EntityDamageByEntityEvent or $event instanceof EntityDamageByChildEntityEvent) {
+			if ($event->getDamager () instanceof Player and $event->getEntity () instanceof Player) {
+				$this->attackQueue [$event->getEntity ()->getName ()] = $event->getDamager ()->getName ();
+			}
+		}
+	}
 	public function onDeath(PlayerDeathEvent $event) {
 		$event->setDrops ( [ ] );
-		if (count ( $this->touchedQueue [$player->getName ()] ) <= 25) foreach ( $this->touchedQueue [$player->getName ()] as $pos ) {
-			$pos = explode ( ".", $pos );
-			$this->setDiamondBlock ( $player, $pos [0], $pos [1], $pos [2] );
+		if (isset ( $this->touchedQueue [$event->getEntity ()->getName ()] )) {
+			if (count ( $this->touchedQueue [$event->getEntity ()->getName ()] ) <= 25) foreach ( $this->touchedQueue [$event->getEntity ()->getName ()] as $pos ) {
+				$pos = explode ( ".", $pos );
+				if (! isset ( $pos [2] )) {
+					echo $pos [0];
+					return;
+				}
+				$this->setBlockPacket ( $event->getEntity (), $pos [0], $pos [1], $pos [2], Block::DIAMOND_BLOCK );
+			}
+			unset ( $this->touchedQueue [$event->getEntity ()->getName ()] );
 		}
-		unset ( $this->touchedQueue [$player->getName ()] );
+		if (isset ( $this->attackQueue [$event->getEntity ()->getName ()] )) {
+			$damager = $this->getServer ()->getPlayerExact ( $this->attackQueue [$event->getEntity ()->getName ()] );
+			if (! $damager instanceof Player) return;
+			$this->KillUpdate ( $damager, $event->getEntity () );
+			unset ( $this->attackQueue [$event->getEntity ()->getName ()] );
+		}
+	}
+	public function KillUpdate($murder, $victim) {
+		if ($victim instanceof Player and $murder instanceof Player) {
+			$mi = "[K" . $this->score [$murder->getName ()] ["kill"] . "+1/D" . $this->score [$murder->getName ()] ["death"] . "]";
+			$vi = "[K" . $this->score [$victim->getName ()] ["kill"] . "/D" . $this->score [$victim->getName ()] ["death"] . "+1]";
+			$this->getServer ()->broadcastMessage ( TextFormat::RED . $murder->getName () . $mi . " " . $victim->getName () . $vi );
+			
+			$this->score [$murder->getName ()] ["kill"] ++;
+			$this->score [$victim->getName ()] ["death"] ++;
+		}
 	}
 	public function initMessage() {
 		$this->saveResource ( "messages.yml", false );
