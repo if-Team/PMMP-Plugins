4c80772ca58814efcdce9db3a638d67233727982
hm <hmkuak@naver.com>
2015-04-06 01:41:58 +0900

    Added EDGE permission 'edge.showingnametag' + EDGE_API

--- EDGE/src/EDGE/EDGE.php
+++ EDGE/src/EDGE/EDGE.php
@@ -11,17 +11,22 @@ use pocketmine\entity\Entity;
 use pocketmine\scheduler\CallbackTask;
 use pocketmine\network\protocol\AddPlayerPacket;
 use pocketmine\network\protocol\RemovePlayerPacket;
+use pocketmine\Player;
+use pocketmine\event\player\PlayerJoinEvent;
 
 class EDGE extends PluginBase implements Listener {
-	public $messages; // 메시지
+	private static $instance = null; // 인스턴스 변수
+	public $messages, $db; // 메시지
 	public $economyAPI = null; // 이코노미 API
 	public $m_version = 1; // 메시지 버전 변수
 	public $packet = [ ]; // 전역 패킷 변수
 	public $packetQueue = [ ]; // 패킷 큐
+	public $specialLineQueue = [ ];
 	public function onEnable() {
 		@mkdir ( $this->getDataFolder () );
 		
 		$this->initMessage ();
+		$this->db = (new Config ( $this->getDataFolder () . "pluginDB.yml", Config::YAML, [ "Format" => "%info%\n%online%\n%mymoney%" ] ))->getAll ();
 		
 		if ($this->getServer ()->getPluginManager ()->getPlugin ( "EconomyAPI" ) != null) {
 			$this->economyAPI = \onebone\economyapi\EconomyAPI::getInstance ();
@@ -31,6 +36,9 @@ class EDGE extends PluginBase implements Listener {
 		}
 		
 		$this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
+		if (self::$instance == null) self::$instance = $this;
+		
+		$this->specialLineQueue ["all"] = [ ];
 		
 		$this->packet ["AddPlayerPacket"] = new AddPlayerPacket ();
 		$this->packet ["AddPlayerPacket"]->clientID = 0;
@@ -61,8 +69,74 @@ class EDGE extends PluginBase implements Listener {
 		}
 	}
 	// ----------------------------------------------------------------------------------
+	public static function getInstance() {
+		return static::$instance;
+	}
+	public function getFormat() {
+		// 포멧을 가져옵니다
+		return $this->db ["Format"];
+	}
+	public function setFormat($input) {
+		// 포멧을 설정합니다
+		return $this->db ["Format"] = $input;
+	}
+	public function getSpecialLines(Player $player = null) {
+		// 추가된 라인들을 가져옵니다
+		if ($player == null) {
+			// 모든유저들에게 추가된 스페셜라인
+			if (isset ( $this->specialLineQueue ["all"] )) {
+				return $this->specialLineQueue ["all"];
+			} else {
+				return false;
+			}
+		} else {
+			// 해당유저가 착용중인 스페셜라인
+			if (isset ( $this->specialLineQueue ["player"] [$player->getName ()] )) {
+				return [ $this->specialLineQueue ["all"],$this->specialLineQueue ["player"] [$player->getName ()] ];
+			} else {
+				return false;
+			}
+		}
+	}
+	public function addSpecialLine(Player $player = null, $text) {
+		// 라인을 추가합니다
+		if ($player == null) {
+			// 모든유저들에게 스페셜라인 추가
+			$this->specialLineQueue ["all"] [] = $text;
+		} else {
+			// 해당유저에게만 스페셜라인 추가
+			$this->specialLineQueue ["player"] [$player->getName ()] [] = $text;
+		}
+	}
+	public function deleteSpecialLine(Player $player = null, $text) {
+		// 스페셜 라인을 가져옵니다
+		if ($player == null) {
+			// 모든유저들에게 스페셜라인 삭제
+			foreach ( $this->specialLineQueue ["all"] as $index => $queue )
+				if ($queue == $text) {
+					unset ( $this->specialLineQueue ["all"] [$index] );
+					break;
+				}
+		} else {
+			// 해당유저에게만 스페셜라인 삭제
+			foreach ( $this->specialLineQueue ["player"] [$player->getName ()] as $index => $queue )
+				if ($queue == $text) {
+					unset ( $this->specialLineQueue ["player"] [$player->getName ()] [$index] );
+					break;
+				}
+		}
+	}
+	public function onJoin(PlayerJoinEvent $event) {
+		$this->specialLineQueue ["player"] [$event->getPlayer ()->getName ()] = [ ];
+	}
+	public function onQuit(PlayerQuitEvent $event) {
+		if (isset ( $this->specialLineQueue ["player"] [$event->getPlayer ()->getName ()] )) unset ( $this->specialLineQueue ["player"] [$event->getPlayer ()->getName ()] );
+		if (isset ( $this->packetQueue [$event->getPlayer ()->getName ()] )) unset ( $this->packetQueue [$event->getPlayer ()->getName ()] );
+	}
+	// ----------------------------------------------------------------------------------
 	public function EDGE() {
 		foreach ( $this->getServer ()->getOnlinePlayers () as $OnlinePlayer ) {
+			if (! $OnlinePlayer->hasPermission ( "edge.showingnametag" )) return;
 			$px = round ( $OnlinePlayer->x );
 			$py = round ( $OnlinePlayer->y );
 			$pz = round ( $OnlinePlayer->z );
@@ -82,17 +156,25 @@ class EDGE extends PluginBase implements Listener {
 			$this->packetQueue [$OnlinePlayer->getName ()] ["z"] = round ( $pz );
 			$this->packetQueue [$OnlinePlayer->getName ()] ["eid"] = Entity::$entityCount ++;
 			
-			$nameTag = $this->get ( "serverinfo" ) . "\n" . $this->get ( "usercount" ) . count ( $this->getServer ()->getOnlinePlayers () ) . "\n" . $this->get ( "mymoney" ) . $this->economyAPI->myMoney ( $OnlinePlayer );
+			$format = str_replace ( "%info%", $this->get ( "serverinfo" ), $this->db ["Format"] );
+			$format = str_replace ( "%online%", $this->get ( "usercount" ) . count ( $this->getServer ()->getOnlinePlayers () ), $format );
+			$format = str_replace ( "%mymoney%", $this->get ( "mymoney" ) . $this->economyAPI->myMoney ( $OnlinePlayer ), $format );
+			
+			foreach ( $this->specialLineQueue ["all"] as $queue )
+				$format .= "\n" . $queue;
+			
+			if (isset ( $this->specialLineQueue ["player"] [$OnlinePlayer->getName ()] )) {
+				foreach ( $this->specialLineQueue ["player"] [$OnlinePlayer->getName ()] as $queue )
+					$format .= "\n" . $queue;
+			}
+			
 			$this->packet ["AddPlayerPacket"]->eid = $this->packetQueue [$OnlinePlayer->getName ()] ["eid"];
-			$this->packet ["AddPlayerPacket"]->username = $nameTag;
+			$this->packet ["AddPlayerPacket"]->username = $format;
 			$this->packet ["AddPlayerPacket"]->x = $px;
 			$this->packet ["AddPlayerPacket"]->y = $py - 3.2;
 			$this->packet ["AddPlayerPacket"]->z = $pz + 0.4;
 			$OnlinePlayer->dataPacket ( $this->packet ["AddPlayerPacket"] );
 		}
-	}
-	public function onQuit(PlayerQuitEvent $event) {
-		if (isset ( $this->packetQueue [$event->getPlayer ()->getName ()] )) unset ( $this->packetQueue [$event->getPlayer ()->getName ()] );
 	}
 }
 ?>
