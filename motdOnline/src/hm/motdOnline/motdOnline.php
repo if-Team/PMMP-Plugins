3efd3ba6f7ff8676e77c9c2353d77eb5e8639b01
hm <hmkuak@naver.com>
2015-04-27 02:38:07 +0900

    motdOnline UPDATE (*code cleaning*)

--- motdOnline/src/hm/motdOnline/motdOnline.php
+++ motdOnline/src/hm/motdOnline/motdOnline.php
@@ -9,17 +9,12 @@ class motdOnline extends PluginBase implements Listener {
 	public $m_version = 1; // 현재 메시지 버전
 	public function onEnable() {
 		$this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
-		$this->getServer ()->getScheduler ()->scheduleRepeatingTask ( new CallbackTask ( [ $this,"updateServerName" ] ), 20 );
+		$this->getServer ()->getScheduler ()->scheduleRepeatingTask ( new UpdateServerNameTask ( $this ), 20 );
 	}
 	public function updateServerName() {
-		$reflection_class = new \ReflectionClass ( "\\pocketmine\\Server" );
-		$property = $reflection_class->getProperty ( 'network' );
-		$property->setAccessible ( true );
-		$network = $property->getValue ( $this->getServer () );
 		$motd = $this->getServer ()->getConfigString ( "motd", "Minecraft: PE Server" );
-		$network->setName ( $motd . " (" . count ( $this->getServer ()->getOnlinePlayers () ) . " / " . $this->getServer ()->getMaxPlayers () . ")" );
+		$this->getServer ()->getNetwork ()->setName ( $motd . " (" . count ( $this->getServer ()->getOnlinePlayers () ) . "/" . $this->getServer ()->getMaxPlayers () . ")" );
 	}
-	// ----------------------------------------------------------------------------------
 }
 
 ?>
