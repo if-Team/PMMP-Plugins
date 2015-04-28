6782979feda53a5172783a0e4671dd81f3c8b5fd
hm <hmkuak@naver.com>
2015-04-29 06:22:43 +0900

    SteveWarn UPDATE

--- SteveWarn/src/hm/SteveWarn/SteveWarn.php
+++ SteveWarn/src/hm/SteveWarn/SteveWarn.php
@@ -4,37 +4,17 @@ namespace hm\SteveWarn;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\network\protocol\ChatPacket;
-use pocketmine\event\player\PlayerChatEvent;
-use pocketmine\event\player\PlayerCommandPreprocessEvent;
-use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\event\player\PlayerKickEvent;
-use Gentleman\task\KickTask;
+use pocketmine\event\player\PlayerLoginEvent;
 
 class SteveWarn extends PluginBase implements Listener {
 	public function onEnable() {
 		$this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
 	}
-	public function PlayerJoin(PlayerJoinEvent $event) {
-		$player = $event->getPlayer ();
-		if (strtolower ( $player->getName () ) == "steve") {
-			$event->setJoinMessage ( "" );
-			$message = "[경고] 닉네임이 Steve입니다, 해당닉네임은 사용불가능합니다\n[경고] 자동으로 킥처리되며 닉네임 변경시 정상이용가능합니다";
-			$player->sendMessage ( $message );
-			$player->sendMessage ( $message );
-			$player->sendMessage ( $message );
-			$this->getServer ()->getScheduler ()->scheduleDelayedTask ( new KickTask ( $player ), 200 );
+	public function onLogin(PlayerLoginEvent $event) {
+		if (strtolower ( $event->getPlayer ()->getName () ) == "steve") {
+			$event->setKickMessage ( "Steve 닉네임은 사용할 수 없습니다 !" );
+			$event->setCancelled ();
 		}
-	}
-	public function onPlayerChat(PlayerChatEvent $event) {
-		if (strtolower ( $event->getPlayer ()->getName () ) == "steve") $event->setCancelled ();
-	}
-	public function onPlayerCommand(PlayerCommandPreprocessEvent $event) {
-		if (strtolower ( $event->getPlayer ()->getName () ) == "steve") $event->setCancelled ();
-	}
-	public function onPlayerKick(PlayerKickEvent $event) {
-		if ($event->getReason () == "기본닉네임") $event->setQuitMessage ( "" );
 	}
 }
 ?>
