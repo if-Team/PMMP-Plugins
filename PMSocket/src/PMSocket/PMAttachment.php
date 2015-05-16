64e9953df588b364133dfb567a624bd4aec21048
일가크 <woni8708@naver.com>
2015-05-16 23:18:45 +0900

    update

---
+++ PMSocket/src/PMSocket/PMAttachment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace PMSocket;
+
+use ifteam\CustomPacket\DataPacket;
+use ifteam\CustomPacket\CPAPI;
+
+
+class PMAttachment extends \ThreadedLoggerAttachment {
+    private $adr = null, $port = null;
+
+    public function LogIn($adr, $port) {
+        $this->adr = $adr;
+    }
+
+    public function LogOut($adr, $port) {
+        $this->port = $port;
+    }
+
+    public final function call($level, $message) {
+        $this->log($level, $message);
+        if ($this->attachment instanceof \ThreadedLoggerAttachment) {
+            $this->attachment->call($level, $message);
+        }
+        if (!($this->adr == null && $this->port == null)) CPAPI::sendPacket(new DataPacket($this->adr, $this->port, $message));
+    }
+}
--- PMSocket/src/PMSocket/PMSocket.php
+++ PMSocket/src/PMSocket/PMSocket.php
@@ -3,33 +3,41 @@ namespace PMSocket;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\item\DiamondAxe;
 use pocketmine\plugin\PluginBase;
 use ifteam\CustomPacket\event\CustomPacketReceiveEvent;
 use ifteam\CustomPacket\DataPacket;
 use ifteam\CustomPacket\CPAPI;
 use pocketmine\utils\TextFormat;
+use PMSocket\PMAttachment;
 
 class PMSocket extends PluginBase implements Listener {
-    public $adr, $port;
+    private $adr, $port;
+    /* @var PMAttachMent */
+    public $att;
+
     public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->info(TextFormat::GOLD . "[PMSocket]enabled");
+
         if ($this->getServer()->getPluginManager()->getPlugin("CustomPacket") === null) {
             $this->getServer()->getLogger()->critical("[CustomPacket Example] CustomPacket plugin was not found. This plugin will be disabled.");
             $this->getServer()->getPluginManager()->disablePlugin($this);
             return;
         }
     }
-    public function onChat(PlayerChatEvent $ev) {
-        CPAPI::sendPacket(new DataPacket($this->adr, $this->port, "[CHAT] "."<".$ev->getPlayer()->getName()."> ".$ev->getMessage()));
-    }
+
     public function onPacketReceive(CustomPacketReceiveEvent $ev) {
         $data = explode(" ", $ev->getPacket()->data);
-        $this->port = $ev->getPacket()->port;
         $this->adr = $ev->getPacket()->address;
-        switch($data[0]) {
+        $this->port = $ev->getPacket()->port;
+        switch ($data[0]) {
             case "connect" :
-                $this->getLogger()->info("Connected in ".$this->adr.":".$this->port);
+                if ($this->adr == null && $this->port == null) $this->getLogger()->info("Connected in " . $this->adr . ":" . $this->port); else {
+                    $this->getLogger()->info("Tried to Connect in " . $this->adr . ":" . $this->port);
+                    CPAPI::sendPacket(new DataPacket($this->adr, $this->port, "cantconnect"));
+                }
+
                 break;
         }
     }
