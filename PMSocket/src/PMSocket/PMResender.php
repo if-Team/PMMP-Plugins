3ef1bc7caba715874c824fcefb9e94a157a20b07
hm <hmkuak@naver.com>
2015-05-17 01:54:55 +0900

    PMSocket - Added Communication

--- PMSocket/src/PMSocket/PMResender.php
+++ PMSocket/src/PMSocket/PMResender.php
@@ -9,28 +9,56 @@ use ifteam\CustomPacket\DataPacket;
 use PMSocket\PMAttachment;
 
 class PMResender implements Listener {
-	private $adr = null, $port = null;
-	public function __construct() {
-		echo "PMResender called\n";
+	private $updateList = [ ];
+	private $password;
+	public function __construct($password) {
+		$this->password = $password;
 	}
 	public function stream($level, $message) {
-		echo "PONG! " . $level . $message . "\n";
+		echo $message . " - PONG!\n"; // TEST
+		foreach ( $this->updateList as $ipport => $data ) {
+			echo "PING! :" . $ipport . "\n"; //
+			$progress = $this->makeTimestamp ( date ( "Y-m-d H:i:s" ) ) - $this->updateList [$ipport] ["lastcontact"];
+			if ($progress > 9) {
+				unset ( $this->updateList [$ipport] );
+				continue;
+			}
+			$ipport = explode ( ":", $ipport );
+			CPAPI::sendPacket ( new DataPacket ( $ipport [0], $ipport [1], json_encode ( [ $this->password,"console",$level,$message ] ) ) );
+		}
 	}
 	public function onPacketReceive(CustomPacketReceiveEvent $ev) {
-		$data = explode ( " ", $ev->getPacket ()->data );
-		switch ($data [0]) {
-			case "connect" :
-				if ($this->adr == null && $this->port == null) {
-					$this->adr = $ev->getPacket ()->address;
-					$this->port = $ev->getPacket ()->port;
-					$this->att->LogIn ( $this->adr, $this->port );
-					echo ( "Connected in " . $this->adr . ":" . $this->port );
-				} else {
-					echo ( "Tried to Connect in " . $this->adr . ":" . $this->port );
-					CPAPI::sendPacket ( new DataPacket ( $this->adr, $this->port, "cantconnect" ) );
-				}
-				break;
+		$data = json_decode ( $ev->getPacket ()->data );
+		if (! is_array ( $data )) {
+			echo "[테스트] 어레이가 아닌 정보 전달됨\n";
+			$ev->getPacket ()->printDump ();
+			return;
 		}
+		if ($data [0] != $this->password) {
+			echo "[테스트] 패스코드가 다른 정보 전달됨\n";
+			var_dump ( $data [0] );
+			return;
+		}
+		switch ($data [1]) {
+			case "update" :
+				$this->updateList [$ev->getPacket ()->address . ":" . $ev->getPacket ()->port] ["lastcontact"] = $this->makeTimestamp ( date ( "Y-m-d H:i:s" ) );
+				CPAPI::sendPacket ( new DataPacket ( $ev->getPacket ()->address, $ev->getPacket ()->port, json_encode ( [ $this->password,"inside","success" ] ) ) );
+				break;
+			case "disconnect" :
+				if (isset ( $this->updateList [$ev->getPacket ()->address . ":" . $ev->getPacket ()->port] ["lastcontact"] )) {
+					unset ( $this->updateList [$ev->getPacket ()->address . ":" . $ev->getPacket ()->port] ["lastcontact"] );
+				}
+				CPAPI::sendPacket ( new DataPacket ( $ev->getPacket ()->address, $ev->getPacket ()->port, json_encode ( [ $this->password,"inside","disconnected" ] ) ) );
+		}
+	}
+	public function makeTimestamp($date) {
+		$yy = substr ( $date, 0, 4 );
+		$mm = substr ( $date, 5, 2 );
+		$dd = substr ( $date, 8, 2 );
+		$hh = substr ( $date, 11, 2 );
+		$ii = substr ( $date, 14, 2 );
+		$ss = substr ( $date, 17, 2 );
+		return mktime ( $hh, $ii, $ss, $mm, $dd, $yy );
 	}
 }
 
--- PMSocket/src/PMSocket/PMSocket.php
+++ PMSocket/src/PMSocket/PMSocket.php
@@ -9,20 +9,59 @@ use pocketmine\plugin\PluginBase;
 use ifteam\CustomPacket\DataPacket;
 use ifteam\CustomPacket\CPAPI;
 use pocketmine\utils\TextFormat;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\Config;
 
 class PMSocket extends PluginBase implements Listener {
-	public $attachment, $resender;
+	public $db;
+	public $attachment = null, $resender = null;
 	public function onEnable() {
-		$this->resender = new PMResender ();
-		$this->attachment = new PMAttachment ( $this->resender );
-		$this->getServer ()->getLogger ()->addAttachment ( $this->attachment );
+		@mkdir ( $this->getDataFolder () ); // 플러그인 폴더생성
+		$this->db = (new Config ( $this->getDataFolder () . "pluginDB.yml", Config::YAML, [ ] ))->getAll ();
 		
 		if ($this->getServer ()->getPluginManager ()->getPlugin ( "CustomPacket" ) === null) {
-			$this->getServer ()->getLogger ()->critical ( "[CustomPacket Example] CustomPacket plugin was not found. This plugin will be disabled." );
+			$this->getServer ()->getLogger ()->critical ( "[PMSocket] CustomPacket plugin was not found. This plugin will be disabled." );
 			$this->getServer ()->getPluginManager ()->disablePlugin ( $this );
 			return;
 		}
-		$this->getServer ()->getPluginManager ()->registerEvents ( $this->resender, $this );
 		$this->getLogger ()->info ( TextFormat::GOLD . "[PMSocket] Enabled" );
+		$this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
+		if (isset ( $this->db ["pass"] )) {
+			$this->registerAttachment ();
+		} else {
+			$this->getLogger ()->info ( TextFormat::GOLD . "[PMSocket] Please register your communicate password" );
+			$this->getLogger ()->info ( TextFormat::GOLD . "[PMSocket] /pmsocket setpass <password>" );
+		}
+	}
+	public function onDisable() {
+		$save = new Config ( $this->getDataFolder () . "pluginDB.yml", Config::YAML );
+		$save->setAll ( $this->db );
+		$save->save ();
+	}
+	public function registerAttachment() {
+		$this->resender = new PMResender ( $this->db ["pass"] );
+		$this->attachment = new PMAttachment ( $this->resender );
+		$this->getServer ()->getLogger ()->addAttachment ( $this->attachment );
+		$this->getServer ()->getPluginManager ()->registerEvents ( $this->resender, $this );
+	}
+	public function onCommand(CommandSender $player, Command $command, $label, Array $args) {
+		if (! isset ( $args [0] )) {
+			$player->sendMessage ( TextFormat::GOLD . "/pmsocket setpass <password>" );
+			return true;
+		}
+		switch (strtolower ( $args [0] )) {
+			case "setpass" :
+				if (! isset ( $args [1] )) {
+					$player->sendMessage ( TextFormat::GOLD . "[PMSocket] /pmsocket setpass <password>" );
+					return true;
+				}
+				array_shift ( $args );
+				$this->db ["pass"] = implode ( " ", $args );
+				$player->sendMessage ( TextFormat::GOLD . "[PMSocket] The password has been registered!" );
+				if ($this->resender == null) $this->registerAttachment ();
+				break;
+		}
+		return true;
 	}
 }
