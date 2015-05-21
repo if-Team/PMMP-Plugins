9848d3035a6c087722966dbd69bf844e51496b5b
hm <hmkuak@naver.com>
2015-05-21 20:53:36 +0900

    Purge - Added isNight() function

--- Purge/src/ifteam/Purge/Purge.php
+++ Purge/src/ifteam/Purge/Purge.php
@@ -19,6 +19,7 @@ use pocketmine\entity\Arrow;
 
 class Purge extends PluginBase implements Listener {
 	public $purgeStarted = false;
+	public $m_version = 1; // 메시지 버전 변수
 	public function onEnable() {
 		@mkdir ( $this->getDataFolder () );
 		
@@ -29,18 +30,43 @@ class Purge extends PluginBase implements Listener {
 	}
 	public function initMessage() {
 		$this->saveResource ( "messages.yml", false );
+		$this->messagesUpdate ( "messages.yml" );
 		$this->messages = (new Config ( $this->getDataFolder () . "messages.yml", Config::YAML ))->getAll ();
 	}
+	public function messagesUpdate($targetYmlName) {
+		$targetYml = (new Config ( $this->getDataFolder () . $targetYmlName, Config::YAML ))->getAll ();
+		if (! isset ( $targetYml ["m_version"] )) {
+			$this->saveResource ( $targetYmlName, true );
+		} else if ($targetYml ["m_version"] < $this->m_version) {
+			$this->saveResource ( $targetYmlName, true );
+		}
+	}
 	public function get($var) {
-		return $this->messages [$this->messages ["default-language"] . "-" . $var];
+		if (isset ( $this->messages [$this->getServer ()->getLanguage ()->getLang ()] )) {
+			$lang = $this->getServer ()->getLanguage ()->getLang ();
+		} else {
+			$lang = "eng";
+		}
+		return $this->messages [$lang . "-" . $var];
 	}
 	public function purgeSchedule() {
-		$time = $this->getServer ()->getDefaultLevel ()->getTime ();
-		$isNight = ($time >= Level::TIME_NIGHT and $time < Level::TIME_SUNRISE);
+		$isNight = $this->isNight ( $this->getServer ()->getDefaultLevel ()->getTime () );
 		if ($this->purgeStarted) {
-			if (! $isNight) $this->purgeStop ();
+			if (! $isNight)
+				$this->purgeStop ();
 		} else {
-			if ($isNight) $this->purgeStart ();
+			if ($isNight)
+				$this->purgeStart ();
+		}
+	}
+	public function isNight($tick) {
+		$totalhour = ($tick / 1000) + 6;
+		$totalday = floor ( $totalhour / 24 );
+		$nowhour = floor ( (floor ( $totalhour ) - $totalday * 24) );
+		if ($nowhour >= 18 or $nowhour < 6) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 	public function purgeStart() {
@@ -57,7 +83,8 @@ class Purge extends PluginBase implements Listener {
 	}
 	public function onDamage(EntityDamageEvent $event) {
 		if ($event instanceof EntityDamageByEntityEvent) {
-			if ($this->purgeStarted) return;
+			if ($this->purgeStarted)
+				return;
 			if ($event->getEntity () instanceof Player and $event->getDamager () instanceof Player) {
 				$event->setCancelled ();
 			}
@@ -67,29 +94,35 @@ class Purge extends PluginBase implements Listener {
 		if ($event->getEntity () instanceof Entity) {
 			foreach ( $event->getEntity ()->getLevel ()->getEntities () as $entity ) {
 				if (isset ( $event->getEntity ()->shootingEntity )) {
-					if ($entity == $event->getEntity ()->shootingEntity) continue;
+					if ($entity == $event->getEntity ()->shootingEntity)
+						continue;
 				}
-				if ($entity instanceof Player) if ($event->getEntity ()->distance ( $entity ) <= 6) {
-					if (! $this->purgeStarted) $event->setCancelled ();
-					break;
-				}
+				if ($entity instanceof Player)
+					if ($event->getEntity ()->distance ( $entity ) <= 6) {
+						if (! $this->purgeStarted)
+							$event->setCancelled ();
+						break;
+					}
 			}
 		}
 	}
 	public function onCombust(EntityCombustEvent $event) {
 		if ($event instanceof EntityCombustByBlockEvent) {
-			if ($this->purgeStarted) return;
+			if ($this->purgeStarted)
+				return;
 			if ($event->getEntity () instanceof Player and $event->getCombuster () instanceof Fire) {
 				$event->setCancelled ();
 			}
 		}
 	}
 	public function message($player, $text = "", $mark = null) {
-		if ($mark == null) $mark = $this->get ( "default-prefix" );
+		if ($mark == null)
+			$mark = $this->get ( "default-prefix" );
 		$player->sendMessage ( TextFormat::DARK_AQUA . $mark . " " . $text );
 	}
 	public function alert($player, $text = "", $mark = null) {
-		if ($mark == null) $mark = $this->get ( "default-prefix" );
+		if ($mark == null)
+			$mark = $this->get ( "default-prefix" );
 		$player->sendMessage ( TextFormat::RED . $mark . " " . $text );
 	}
 }
