8fdfe9598eb61f9f66a51903d42f2b1ce4ca80d0
hm <hmkuak@naver.com>
2015-06-18 14:05:05 +0900

    Company WORK 40% UPLOAD

---
+++ Company/src/ifteam/Company/Company.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace ifteam\Company;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\command\PluginCommand;
+use pocketmine\utils\TextFormat;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class Company extends PluginBase implements Listener {
+	private static $instance = null; // 인스턴스 변수
+	public $economyAPI = null;
+	public $messages, $db; // 메시지 변수, DB변수
+	public $m_version = 1; // 현재 메시지 버전
+	public function onEnable() {
+		@mkdir ( $this->getDataFolder () ); // 플러그인 폴더생성
+		
+		$this->initMessage (); // 기본언어메시지 초기화
+		                       
+		// YAML 형식의 DB생성 후 불러오기
+		$this->db = new Company_Database ( $this->getDataFolder () . "company_db.yml", $this );
+		
+		// 플러그인의 인스턴스 정의
+		if (self::$instance == null)
+			self::$instance = $this;
+			
+			// 플러그인의 명령어 등록
+			// $this->registerCommand ( "명령어이름", "등록실패시 보조 명령어이름", "퍼미션명", "명령어설명", "쓰는법" );
+			
+		// 이코노미 API 이용
+		if ($this->getServer ()->getPluginManager ()->getPlugin ( "EconomyAPI" ) != null) {
+			$this->economyAPI = \onebone\economyapi\EconomyAPI::getInstance ();
+		} else {
+			$this->getLogger ()->error ( $this->get ( "there-are-no-economyapi" ) );
+			$this->getServer ()->getPluginManager ()->disablePlugin ( $this );
+		}
+		
+		// 서버이벤트를 받아오게끔 플러그인 리스너를 서버에 등록
+		$this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
+	}
+	public function onCommand(CommandSender $player, Command $command, $label, Array $args) {
+		switch (strtolower ( $command->getName () )) {
+			case $this->get ( "commands-buy" ) :
+				break;
+		}
+		return true;
+	}
+	public function get($var) {
+		if (isset ( $this->messages [$this->getServer ()->getLanguage ()->getLang ()] )) {
+			$lang = $this->getServer ()->getLanguage ()->getLang ();
+		} else {
+			$lang = "eng";
+		}
+		return $this->messages [$lang . "-" . $var];
+	}
+	public static function getInstance() {
+		return static::$instance;
+	}
+	public function initMessage() {
+		$this->saveResource ( "messages.yml", false );
+		$this->messagesUpdate ( "messages.yml" );
+		$this->messages = (new Config ( $this->getDataFolder () . "messages.yml", Config::YAML ))->getAll ();
+	}
+	public function messagesUpdate($targetYmlName) {
+		$targetYml = (new Config ( $this->getDataFolder () . $targetYmlName, Config::YAML ))->getAll ();
+		if (! isset ( $targetYml ["m_version"] )) {
+			$this->saveResource ( $targetYmlName, true );
+		} else if ($targetYml ["m_version"] < $this->m_version) {
+			$this->saveResource ( $targetYmlName, true );
+		}
+	}
+	public function registerCommand($name, $permission, $description = "", $usage = "") {
+		$commandMap = $this->getServer ()->getCommandMap ();
+		$command = new PluginCommand ( $name, $this );
+		$command->setDescription ( $description );
+		$command->setPermission ( $permission );
+		$command->setUsage ( $usage );
+		$commandMap->register ( $name, $command );
+	}
+	public function message($player, $text = "", $mark = null) {
+		if ($mark == null)
+			$mark = $this->get ( "default-prefix" );
+		$player->sendMessage ( TextFormat::DARK_AQUA . $mark . " " . $text );
+	}
+	public function alert($player, $text = "", $mark = null) {
+		if ($mark == null)
+			$mark = $this->get ( "default-prefix" );
+		$player->sendMessage ( TextFormat::RED . $mark . " " . $text );
+	}
+	public function onDisable() {
+		$save = new Config ( $this->getDataFolder () . "pluginDB.yml", Config::YAML );
+		$save->setAll ( $this->db );
+		$save->save ();
+	}
+	// ----------------------------------------------------------------------------------
+}
+
+?>
