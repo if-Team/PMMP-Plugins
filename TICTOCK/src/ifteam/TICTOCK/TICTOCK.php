0ec47d68e8e30b2b93a68a1c34dc20f2d5555f9b
hm <hmkuak@naver.com>
2015-06-20 19:49:16 +0900

    TICTOCK - Fixed

--- TICTOCK/src/ifteam/TICTOCK/TICTOCK.php
+++ TICTOCK/src/ifteam/TICTOCK/TICTOCK.php
@@ -17,33 +17,46 @@ class TICTOCK extends PluginBase implements Listener {
 	public function onBreak(BlockBreakEvent $event) {
 		$player = $event->getPlayer ();
 		
-		if ($player->isCreative ()) return;
+		if ($player->isCreative ())
+			return;
+		if ($event->getBlock ()->getBreakTime ( $event->getItem () ) <= 0.1)
+			return;
+		
 		$time = round ( microtime ( true ) * 1000 );
 		
-		if ($event->getBlock ()->getBreakTime ( $event->getItem () ) <= 0.1) return;
+		if (! isset ( $this->tictock [spl_object_hash ( $player->getName () )] )) {
+			$this->tictock [spl_object_hash ( $player->getName () )] = $time;
+			return;
+		}
 		
-		if (($time - $this->tictock [$player->getName ()]) <= 100) {
-			if (! isset ( $this->kick [$player->getName ()] )) {
-				$this->kick [$player->getName ()] = 1;
+		if (($time - $this->tictock [spl_object_hash ( $player->getName () )]) <= 60) {
+			if (! isset ( $this->kick [spl_object_hash ( $player->getName () )] )) {
+				$this->kick [spl_object_hash ( $player->getName () )] = 1;
 			} else {
-				$this->kick [$player->getName ()] ++;
-			}
-			if ($this->kick > 2) {
-				$player->kick ( "파괴자모드 감지" );
-				if (isset ( $this->kick [$player->getName ()] )) unset ( $this->kick [$player->getName ()] );
+				$this->kick [spl_object_hash ( $player->getName () )] ++;
+				if ($this->kick [spl_object_hash ( $player->getName () )] > 2) {
+					$player->kick ( "파괴자모드 감지" );
+					unset ( $this->kick [spl_object_hash ( $player->getName () )] );
+				}
 			}
 			$event->setCancelled ();
 		} else {
-			if (isset ( $this->kick [$player->getName ()] )) unset ( $this->kick [$player->getName ()] );
+			if (isset ( $this->kick [spl_object_hash ( $player->getName () )] ))
+				unset ( $this->kick [spl_object_hash ( $player->getName () )] );
 		}
-		$this->tictock [$player->getName ()] = $time;
+		$this->tictock [spl_object_hash ( $player->getName () )] = $time;
 	}
 	public function onJoin(PlayerJoinEvent $event) {
-		$this->tictock [$event->getPlayer ()->getName ()] = round ( microtime ( true ) * 1000 );
+		$this->tictock [spl_object_hash ( $player->getName () )] = round ( microtime ( true ) * 1000 );
 	}
 	public function onQuit(PlayerQuitEvent $event) {
 		$player = $event->getPlayer ();
-		if (isset ( $this->tictock [$player->getName ()] )) unset ( $this->tictock [$player->getName ()] );
+		
+		if (isset ( $this->tictock [spl_object_hash ( $player->getName () )] ))
+			unset ( $this->tictock [spl_object_hash ( $player->getName () )] );
+		
+		if (isset ( $this->kick [spl_object_hash ( $player->getName () )] ))
+			unset ( $this->kick [spl_object_hash ( $player->getName () )] );
 	}
 }
 
