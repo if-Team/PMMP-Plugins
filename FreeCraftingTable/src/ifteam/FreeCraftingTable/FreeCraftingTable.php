73fba4c085ab403950876e7ae7f49cdb181738e0
ChalkPE <amato0617@gmail.com>
2015-06-20 16:52:17 +0900

    Removed duplicated codes

--- FreeCraftingTable/src/ifteam/FreeCraftingTable/FreeCraftingTable.php
+++ FreeCraftingTable/src/ifteam/FreeCraftingTable/FreeCraftingTable.php
@@ -2,6 +2,7 @@
 
 namespace ifteam\FreeCraftingTable;
 
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
@@ -9,22 +10,24 @@ use pocketmine\item\Item;
 use pocketmine\event\player\PlayerRespawnEvent;
 
 class FreeCraftingTable extends PluginBase implements Listener {
-	public function onEnable() {
-		$this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
-	public function onJoin(PlayerJoinEvent $event) {
-		$player = $event->getPlayer ();
-		foreach ( $player->getInventory ()->getContents () as $item ) {
-			if ($item->getID () == Item::CRAFTING_TABLE) return;
-		}
-		$player->getInventory ()->addItem ( Item::get ( Item::CRAFTING_TABLE ) );
+
+    public function giveCraftingTable(Player $player){
+        foreach($player->getInventory()->getContents() as $item){
+            if($item->getId() === Item::CRAFTING_TABLE) return;
+        }
+
+        $player->getInventory()->addItem(Item::get(Item::CRAFTING_TABLE));
+    }
+
+	public function onJoin(PlayerJoinEvent $event){
+        $this->giveCraftingTable($event->getPlayer());
 	}
-	public function checkRespawn(PlayerRespawnEvent $event) {
-		$player = $event->getPlayer ();
-		foreach ( $player->getInventory ()->getContents () as $item ) {
-			if ($item->getID () == Item::CRAFTING_TABLE) return;
-		}
-		$player->getInventory ()->addItem ( Item::get ( Item::CRAFTING_TABLE ) );
+
+	public function onRespawn(PlayerRespawnEvent $event){
+		$this->giveCraftingTable($event->getPlayer());
 	}
 }
 ?>
